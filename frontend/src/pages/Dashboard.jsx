d660cc648e78f355bdecaa0b6341e14b7088cf38
Dinesh Choudhary <dineshchoudhary182005@gmail.com>
2026-05-21 21:58:13 +0530

    Change Heading

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -41,7 +41,7 @@ export default function Dashboard() {
       <div className="mb-8">
 
         <h1 className="text-5xl font-bold">
-          NexusOps Control Center 
+          NexusOp Control Center 
         </h1>
 
         <p className="text-slate-400 mt-3 text-lg">
